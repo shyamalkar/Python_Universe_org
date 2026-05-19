0394564e8ae31d2dacbf223889a36ebd314be9ee
Shyamal kar <shyamalkar430@gmail.com>
2026-05-19 08:54:54 +0530

    Update area_of_triangle.py

--- area_of_triangle.py
+++ area_of_triangle.py
@@ -7,6 +7,7 @@ Area = square root of (s * (s - a) * (s - b) * (s - c))
 a = float(input("Enter first side: "))    
 b = float(input("Enter second side: "))
 c = float(input("Enter third side: ")) 
+
  
 s = ( a + b + c ) / 2
 area = ( s * ( s - a) * (s - b ) * (s - c)) ** 0.5
