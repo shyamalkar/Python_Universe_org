b8233464eedd9356f940c5495ba5d391c470131d
Shyamal kar <shyamalkar430@gmail.com>
2026-05-19 22:06:30 +0530

    Update touple_intro.py

--- touple_intro.py
+++ touple_intro.py
@@ -1,4 +1,4 @@
-# Tuple
+ # Tuple
 #(item1, item2, ...)
 # sequence of items as a collectiont_1 = ("Python", 10, 1.5, True, [1, 2, 4], (10, 20))
 print("In short tuple means: Tuple is a list but if create one time but never change immutable. ")
