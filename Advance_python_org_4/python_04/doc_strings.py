b4efd116f1a951a7afa8b5c776c2c9ce669eade8
Shyamal kar <shyamalkar430@gmail.com>
2026-05-22 08:04:31 +0530

    Update doc_strings.py

--- Advance_python_org_4/python_04/doc_strings.py
+++ Advance_python_org_4/python_04/doc_strings.py
@@ -4,4 +4,6 @@ def infor():
     Here is the news today , You can see what is the inside in this function using just , help function
     """
     return None
-help(infor)
+help(infor) 
+
+#write it about what is the coding meaning .
