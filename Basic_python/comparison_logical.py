1bea0121db1189ff69fb5bc98690edf81958349d
Shyamal kar <shyamalkar430@gmail.com>
2026-06-13 10:50:31 +0530

    Update comparison_logical.py

--- Basic_python/comparison_logical.py
+++ Basic_python/comparison_logical.py
@@ -10,6 +10,7 @@ print(a < b)    # False # less than ?
 print(a == b)   # False # equal ?
 print(a != b)   # True # Not equal
 print(a >= b)   # True # grather than or equal
+# From day one 
 print(a <= b)   # False # less than  or equal
 
 #Logical Operators
